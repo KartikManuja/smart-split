640eea31d8d9111ddaa84e0e6d13eebcc2c425c9
Kartik Manuja <kartikmanuja393@gmail.com>
2026-08-22 15:28:46 +0530

    Point frontend to deployed backend URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://smart-split-1-yiyk.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
